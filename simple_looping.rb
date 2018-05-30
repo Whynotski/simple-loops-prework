bee2de81decfb18ef9fdba9cac41bfd6bce1504a
Laura Adam <laurawynosky@gmail.com>
2018-05-30 21:36:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,4 +1,4 @@
-learn# REMEMBER: print your output to the terminal using 'puts'
+
 #1
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
@@ -11,21 +11,18 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
   loop do
-    counter +=1
     puts phrase
-    if counter >=10 
-      break
-    end
+     counter += 1
+   break if counter == number_of_times
   end
 end  
   
 #2 X
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-   
-	10.times do
-		puts phrase
   phrase = "Welcome to Flatiron School's Web Development Course!"
+	number_of_times.times do
+		puts phrase
   end
 end
 
@@ -33,11 +30,11 @@ end
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
-  number_of_times =0
-  while number_of_times< 10
-  puts phrase
-  number_of_times+=1 
+  count = 1 
+  while count <= number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts phrase
+  count += 1 
   end
 end
 
@@ -45,11 +42,11 @@ end
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  number_of_times =0
-  until number_of_times == 10
-  puts phrase
-  number_of_times  +=1 
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  count = 1
+  until count == number_of_times
+    puts phrase
+    count += 1 
   end
 end
   
@@ -58,8 +55,8 @@ def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for counter in 1..10 do
-  puts phrase
-  number_of_times+=1 
+  for count in 1..{number_of_times} do
+    puts phrase
+    count += (number_of_times)
   end
 end
